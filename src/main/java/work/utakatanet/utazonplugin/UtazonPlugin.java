10e796680be868c655aabe87e2c0380c31e944dd
MyokoJP <myonmyon26@gmail.com>
2023-07-22 20:44:51 +0900

    Initial commit

--- src/main/java/work/utakatanet/utazonplugin/UtazonPlugin.java
+++ src/main/java/work/utakatanet/utazonplugin/UtazonPlugin.java
@@ -1,8 +1,11 @@
 package work.utakatanet.utazonplugin;
 
+import com.github.kuripasanda.economyutilsapi.api.EconomyUtilsApi;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class UtazonPlugin extends JavaPlugin {
+
+    public static final EconomyUtilsApi ecoApi = EconomyUtilsAPI.api;
 
     @Override
     public void onEnable() {

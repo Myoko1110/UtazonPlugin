9b0c6fa424653264035035fba5be1d9d6bcdf3c7
MyokoJP <myonmyon26@gmail.com>
2023-07-22 19:54:19 +0900

    Initial commit

---
+++ src/main/java/work/utakatanet/utazonplugin/UtazonPlugin.java
@@ -0,0 +1,17 @@
+package work.utakatanet.utazonplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UtazonPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

db4b6c34c729dc5be748490ec1bf01f6e6dfb8ac
MyokoJP <myonmyon26@gmail.com>
2023-08-02 17:34:13 +0900

    [Add] ソケットサーバーの実装

--- src/main/java/work/utakatanet/utazonplugin/UtazonPlugin.java
+++ src/main/java/work/utakatanet/utazonplugin/UtazonPlugin.java
@@ -1,20 +1,36 @@
 package work.utakatanet.utazonplugin;
 
-import com.github.kuripasanda.economyutilsapi.api.EconomyUtilsApi;
+//import com.github.kuripasanda.economyutilsapi.api.EconomyUtilsApi;
 import org.bukkit.plugin.java.JavaPlugin;
+import work.utakatanet.utazonplugin.utils.SocketServer;
 
 public final class UtazonPlugin extends JavaPlugin {
 
-    public static final EconomyUtilsApi ecoApi = EconomyUtilsAPI.api;
+    // public static final EconomyUtilsApi ecoApi = EconomyUtilsAPI.api;
+    private static UtazonPlugin plugin;
+    private SocketServer socketServer;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
 
+        getLogger().info("UtazonPluginが有効になりました。");
+        saveDefaultConfig();
+
+        socketServer = new SocketServer();
+        socketServer.start();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+        if (socketServer != null) {
+            socketServer.stopServer();
+        }
+    }
+
+    public static UtazonPlugin getPlugin() {
+        return plugin;
     }
 }
---
+++ src/main/java/work/utakatanet/utazonplugin/utils/SocketServer.java
@@ -0,0 +1,93 @@
+package work.utakatanet.utazonplugin.utils;
+
+import work.utakatanet.utazonplugin.UtazonPlugin;
+
+import java.io.*;
+import java.net.*;
+import java.util.UUID;
+
+public class SocketServer implements Runnable {
+
+    public static UtazonPlugin utazonPlugin = UtazonPlugin.getPlugin();
+
+    private ServerSocket serverSocket;
+    private boolean isRunning = false;
+
+    public void start() {
+        new Thread(this).start();
+        utazonPlugin.getLogger().info("socketサーバーを起動しました。");
+    }
+
+    @Override
+    public void run() {
+        try {
+            serverSocket = new ServerSocket(GetSocketPort());
+            isRunning = true;
+
+            while (isRunning) {
+                Socket clientSocket = serverSocket.accept();
+                handleClient(clientSocket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopServer() {
+        isRunning = false;
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleClient(Socket clientSocket) throws IOException {
+
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
+        try {
+            inputStream = clientSocket.getInputStream();
+            outputStream = clientSocket.getOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                String receivedData = new String(buffer, 0, bytesRead);
+
+                try{
+                    UUID uuid = UUID.fromString(receivedData);
+                    utazonPlugin.getLogger().info("プレイヤーの残高がsocketから参照されました。 参照UUID:" + receivedData);
+
+                    String responseData = receivedData.toUpperCase();
+                    outputStream.write(responseData.getBytes());
+                    outputStream.flush();
+
+                } catch (IllegalArgumentException e){
+                    outputStream.write("Invalid UUID".getBytes());
+                    outputStream.flush();
+                }
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            if (clientSocket != null) {
+                clientSocket.close();
+            }
+        }
+    }
+
+    public static int GetSocketPort(){
+        return utazonPlugin.getConfig().getInt("socket.port");
+    }
+}
